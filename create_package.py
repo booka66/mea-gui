64c3f2bb48f7547f33713ed8c587c2713760a6b5
booka66 <jacobbcahoon@gmail.com>
2024-07-09 10:18:14 -0600

    create package update

--- create_package.py
+++ create_package.py
@@ -3,17 +3,29 @@ import sys
 
 
 def main():
+    pyinstaller_command = """ pyinstaller --noconfirm --onefile --windowed main.py --icon=icon.ico --add-data "SzDetectCat.m:." --add-data "save_channel_to_mat.m:." --add-data "getChs.m:." --add-data "get_cat_envelop.m:." --additional-hooks-dir "hooks" --add-data "*.m:."
+        """
     if sys.platform == "darwin":
-        command = """
-            sudo pyinstaller --noconfirm --onefile --windowed main.py --icon=icon.icns --add-data "SzDetectCat.m:." --add-data "save_channel_to_mat.m:." --add-data "getChs.m:." --add-data "get_cat_envelop.m:." --additional-hooks-dir "hooks" --add-data "*.m:."
-        """
-    else:
-        command = """
-            pyinstaller --noconfirm --onefile --windowed main.py --icon=icon.ico --add-data "SzDetectCat.m:." --add-data "save_channel_to_mat.m:." --add-data "getChs.m:." --add-data "get_cat_envelop.m:." --additional-hooks-dir "hooks" --add-data "*.m:."
-        """
+        pyinstaller_command = "sudo " + pyinstaller_command
 
-    process = subprocess.Popen(command, shell=True)
-    process.wait()
+        package_commands = [
+            "rm -rf package_root/Applications/MEA\ GUI.app",
+            "cp -R dist/main.app package_root/Applications/MEA\ GUI.app ",
+            "cp fonts/HackNerdFontMono-Regular.ttf package_root/Library/Fonts ",
+            "pkgbuild --root package_root --identifier com.booka66.meagui MEA_GUI_MacOS.pkg",
+        ]
+    else:
+        package_commands = [
+            # Run the Inno Setup Compiler on your .iss file
+            r'"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" /Q MEA_GUI_Installer.iss'
+        ]
+
+    pyinstaller_process = subprocess.Popen(pyinstaller_command, shell=True)
+    pyinstaller_process.wait()
+
+    for command in package_commands:
+        package_process = subprocess.Popen(command, shell=True)
+        package_process.wait()
 
     print("Package created successfully! 🎉🎉🎉")
 
