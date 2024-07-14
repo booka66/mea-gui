7fb2bd1ada9c5dc1a8e81b1c2f3d5a7c4db8106c
booka66 <jacobbcahoon@gmail.com>
2024-07-14 01:17:03 -0600

    please

--- setup.py
+++ setup.py
@@ -3,42 +3,34 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 import pybind11
 import os
 import sys
+import h5py
 
 
 def print_debug(message):
     print(f"DEBUG: {message}")
 
 
-# HDF5 paths
+# Get HDF5 info from h5py
+hdf5_version = h5py.version.hdf5_version
+hdf5_dir = h5py.get_config().hdf5_dir
+print_debug(f"HDF5 version: {hdf5_version}")
+print_debug(f"HDF5 dir: {hdf5_dir}")
+
 if sys.platform == "win32":
-    hdf5_dir = r"D:\Users\booka66\Desktop\HDF5-1.14.4-win64"
     hdf5_include_dir = os.path.join(hdf5_dir, "include")
     hdf5_lib_dir = os.path.join(hdf5_dir, "lib")
-    vs_dir = r"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.40.33807"
-    win_kit_dir = r"C:\Program Files (x86)\Windows Kits\10\lib\10.0.22621.0"
 else:
     raise NotImplementedError("This script is currently set up for Windows only.")
 
 print_debug(f"HDF5 include dir: {hdf5_include_dir}")
 print_debug(f"HDF5 lib dir: {hdf5_lib_dir}")
 
-# List all files in the HDF5 lib directory
-print_debug("Files in HDF5 lib directory:")
-for file in os.listdir(hdf5_lib_dir):
-    print_debug(f"  {file}")
-
-# Explicitly specify the libraries
-libraries = ["libhdf5_cpp", "libhdf5", "libszaec", "libzlib"]
-
 # Compile and link arguments
 compile_args = ["/std:c++17", "/EHsc", "/bigobj", f"/I{hdf5_include_dir}"]
-link_args = [
-    f"/LIBPATH:{hdf5_lib_dir}",
-    "/NODEFAULTLIB:libcmt.lib",
-    f"/LIBPATH:{os.path.join(vs_dir, 'lib', 'x64')}",
-    f"/LIBPATH:{os.path.join(win_kit_dir, 'ucrt', 'x64')}",
-    f"/LIBPATH:{os.path.join(win_kit_dir, 'um', 'x64')}",
-]
+link_args = [f"/LIBPATH:{hdf5_lib_dir}"]
+
+# Libraries to link against
+libraries = ["hdf5_cpp", "hdf5"]
 
 print_debug(f"Compile args: {compile_args}")
 print_debug(f"Link args: {link_args}")
@@ -69,4 +61,5 @@ setup(
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
     python_requires=">=3.6",
+    install_requires=["h5py"],
 )
