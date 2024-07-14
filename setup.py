0cd21fbc95eb406524a2e2a2f86e65a676f9e1f1
booka66 <jacobbcahoon@gmail.com>
2024-07-14 00:39:53 -0600

    better setup for windows

--- setup.py
+++ setup.py
@@ -2,11 +2,35 @@ from setuptools import setup, Extension
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 import pybind11
 import os
+import sys
 
-# HDF5 paths for Homebrew installation
-hdf5_dir = "/opt/homebrew/Cellar/hdf5/1.14.3_1"
-hdf5_include_dir = os.path.join(hdf5_dir, "include")
-hdf5_lib_dir = os.path.join(hdf5_dir, "lib")
+# HDF5 paths
+if sys.platform == "darwin":
+    hdf5_dir = "/opt/homebrew/Cellar/hdf5/1.14.3_1"
+    hdf5_include_dir = os.path.join(hdf5_dir, "include")
+    hdf5_lib_dir = os.path.join(hdf5_dir, "lib")
+elif sys.platform == "win32":
+    # Replace these with your actual HDF5 paths on Windows
+    hdf5_include_dir = r"D:\Users\booka66\Desktop\HDF5-14.4-win64\include"
+    hdf5_lib_dir = r"D:\Users\booka66\Desktop\HDF5-14.4-win64\lib"
+else:
+    hdf5_include_dir = ""
+    hdf5_lib_dir = ""
+
+# Common compile and link arguments
+compile_args = ["-std=c++17"]
+link_args = []
+
+# Platform-specific settings
+if sys.platform == "win32":
+    compile_args = ["/std:c++17"]  # MSVC equivalent of -std=c++17
+    libraries = ["libhdf5_cpp", "libhdf5"]
+else:
+    libraries = ["hdf5_cpp", "hdf5"]
+
+# Add include and lib directories to compile and link args
+compile_args.append(f"-I{hdf5_include_dir}")
+link_args.append(f"-L{hdf5_lib_dir}")
 
 ext_modules = [
     Pybind11Extension(
@@ -17,9 +41,9 @@ ext_modules = [
             hdf5_include_dir,
         ],
         library_dirs=[hdf5_lib_dir],
-        libraries=["hdf5_cpp", "hdf5"],
-        extra_compile_args=["-std=c++17", f"-I{hdf5_include_dir}"],
-        extra_link_args=[f"-L{hdf5_lib_dir}"],
+        libraries=libraries,
+        extra_compile_args=compile_args,
+        extra_link_args=link_args,
     ),
 ]
 
--- sz_se_detect.cpp
+++ sz_se_detect.cpp
@@ -10,7 +10,6 @@
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <string>
 #include <thread>
 #include <vector>
 
