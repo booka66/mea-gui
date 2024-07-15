dac7ee3a81cf87be0e80a94f917c12cccd99be42
booka66 <jacobbcahoon@gmail.com>
2024-07-15 09:35:14 -0600

    please

--- wintest.py
+++ wintest.py
@@ -1,7 +1,15 @@
 import sz_se_detect
+import numpy as np
 
 # Assuming processAllChannels is a function in your module
-result = sz_se_detect.processAllChannels(
+results = sz_se_detect.processAllChannels(
     "D:\\Users\\booka66\\Downloads\\5_13_24_slice1B_resample_100.brw", True
 )
-print(result)
+
+for result in results:
+    signal = np.array(result.signal, dtype=np.float16).squeeze()
+    name = (result.Row, result.Col)
+    SzTimes = np.array(result.result.SzTimes)
+    SETimes = np.array(result.result.SETimes)
+    DischargeTimes = np.array(result.result.DischargeTimes)
+    print(signal, name, SzTimes, SETimes, DischargeTimes)
