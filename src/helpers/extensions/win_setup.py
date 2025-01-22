0c95b2634f59a5932c2f7b778d59d914a6ce9d8b
Jacob Cahoon <booka66@jacobs-mbp.lan>
2025-01-21 20:39:59 -0700

    winsetup update

--- src/helpers/extensions/win_setup.py
+++ src/helpers/extensions/win_setup.py
@@ -3,15 +3,15 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 import pybind11
 import os
 
-# HDF5 paths for CI environment
-hdf5_dir = os.path.join(os.environ.get("GITHUB_WORKSPACE", ""), "HDF5-1.14.4-win64")
+# HDF5 paths for CI environment - Updated version number
+hdf5_dir = os.path.join(os.environ.get("GITHUB_WORKSPACE", ""), "HDF5-1.14.5-win64")
 hdf5_include_dir = os.path.join(hdf5_dir, "include")
 hdf5_lib_dir = os.path.join(hdf5_dir, "lib")
 
 # Add HDF5 bin directory to PATH (for dynamic libraries)
 os.environ["PATH"] = os.path.join(hdf5_dir, "bin") + os.pathsep + os.environ["PATH"]
 
-# Choose between static and dynamic libraries
+# Choose between dynamic and dynamic libraries
 use_dynamic = True  # Set to False for static libraries
 if use_dynamic:
     compile_args = ["/std:c++17", f"/I{hdf5_include_dir}", "/DH5_BUILT_AS_DYNAMIC_LIB"]
@@ -39,7 +39,7 @@ ext_modules = [
         "signal_analyzer",
         ["signal_analyzer.cpp"],
         include_dirs=[pybind11.get_include()],
-        extra_compile_args=["/std=c++17"],
+        extra_compile_args=["/std=c++17"],  # Fixed to use /std:c++17 for consistency
     ),
 ]
 
